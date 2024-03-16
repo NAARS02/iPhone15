6034791f5015e1ad37c008d871b67b335fa482cd
Naarayana Kumar <naarayana.kumar@gmail.com>
2024-03-16 19:02:51 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    project: "javascript-react"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
